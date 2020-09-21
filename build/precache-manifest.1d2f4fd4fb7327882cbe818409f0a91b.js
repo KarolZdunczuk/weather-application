6bacf13fc0c79a83ea3bc29d239c90b7d6a9a8c3
Karol Zduńczuk <berimbau28@gmail.com>
2020-09-21 22:02:48 +0200

    commit on master

---
+++ build/precache-manifest.1d2f4fd4fb7327882cbe818409f0a91b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f78ce63b21c464f8e68d22236f2703d",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "0d30675ca50049afd2b2",
+    "url": "/Weather-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fd944246fa604e73117d",
+    "url": "/Weather-app/static/css/main.0ce36893.chunk.css"
+  },
+  {
+    "revision": "0d30675ca50049afd2b2",
+    "url": "/Weather-app/static/js/2.9ef4ed0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-app/static/js/2.9ef4ed0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd944246fa604e73117d",
+    "url": "/Weather-app/static/js/main.d445ea7b.chunk.js"
+  },
+  {
+    "revision": "a2861103e0194e8c138c",
+    "url": "/Weather-app/static/js/runtime-main.1d012fff.js"
+  }
+]);
